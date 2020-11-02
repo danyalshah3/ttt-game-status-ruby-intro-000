72ddbd2f1ad1998f0ee40f0dc80d35fad51b4968
Muhammad mahdi <danyalm50@gmail.com>
2020-11-02 16:09:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,10 +26,10 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
      return win_combination
-   else
-      false
     end
 end
+else 
+  return false
   end
 
 def full?(board)
